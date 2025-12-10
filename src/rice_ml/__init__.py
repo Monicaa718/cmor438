03e0e4ab3aee4d0b95c349c5346da6d860d5ec7d
Amy Xing <ax15@rice.edu>
2025-12-10 13:36:21 -0600

    Added Logistic Regression source code, unit tests, example notebook, and updated documentation

--- src/rice_ml/__init__.py
+++ src/rice_ml/__init__.py
@@ -1,2 +1,5 @@
 from .processing import *
 from .supervised_learning import *
+
+from .supervised_learning.linear_regression import LinearRegression
+from .supervised_learning.logistic_regression import LogisticRegression
