246c0cb98494d1146379530b0ae3cc97cf04e690
sw107-source <sw107@rice.edu>
2025-12-13 19:53:36 +0800

    Updated example of KNN with own source code

--- src/rice_ml/__init__.py
+++ src/rice_ml/__init__.py
@@ -3,3 +3,5 @@ from .supervised_learning import *
 
 from .supervised_learning.linear_regression import LinearRegression
 from .supervised_learning.logistic_regression import LogisticRegression
+
+from .supervised_learning.k_nearest_neighbors import KNNClassifier
