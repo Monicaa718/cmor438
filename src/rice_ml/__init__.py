ea433c4cf8e79e811851d1b5466f04dddd4caa14
sw107-source <sw107@rice.edu>
2025-12-16 23:19:53 +0800

    moved source code of pca to unsupervised learning

--- src/rice_ml/__init__.py
+++ src/rice_ml/__init__.py
@@ -5,3 +5,4 @@ from .supervised_learning.linear_regression import LinearRegression
 from .supervised_learning.logistic_regression import LogisticRegression
 
 from .supervised_learning.k_nearest_neighbors import KNNClassifier
+from .unsupervised_learning.pca import PCA
